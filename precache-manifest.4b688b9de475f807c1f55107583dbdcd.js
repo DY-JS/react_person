7f1efcd317f77a7d788e3a1f9a38626a47b525f2
DY-JS <delo1604@ukr.net>
2021-10-11 13:33:11 +0300

    Updates

---
+++ precache-manifest.4b688b9de475f807c1f55107583dbdcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d58f8231ee81970ebc35af39aa7ac96",
+    "url": "./index.html"
+  },
+  {
+    "revision": "69843f496ecf421d8217",
+    "url": "./static/css/main.480643a3.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69843f496ecf421d8217",
+    "url": "./static/js/main.b26597cd.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
